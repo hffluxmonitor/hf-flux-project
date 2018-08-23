9a35a6674804517d6d426f898cf49294f8c95329
hffluxmonitor <39916010+hffluxmonitor@users.noreply.github.com>
2018-08-23 15:43:50 -0400

    Update add_qc_flag w/ working code

--- add_qc_flag.py
+++ add_qc_flag.py
@@ -5,7 +5,6 @@ Created on Mon Aug 20 14:55:58 2018
 """
 import os
 import pandas as pd
-import numpy as np
 
 username = os.getlogin()
 
@@ -15,18 +14,14 @@ df.end_date = pd.to_datetime(df.end_date,yearfirst = True)
 df_flux = pd.read_csv('C://Users/'+username+'/Dropbox/Obrist Lab/HarvardForestData/flux_data.csv')
 df_flux['datetime'] = pd.to_datetime(df_flux['datetime'])
 df_flux['qc_flag'] = 0
-qc = []
 
-for j, row in df_flux.iterrows():
-    for i in range(0,2):
-            sd = df.iloc[i].start_date
-            ed = df.iloc[i].end_date
-            df_flux.iloc[j]['qc_flag'] = np.where(((row['datetime'] > sd) & (row['datetime'] <= ed)),1,0)
-
-#This creates a list with two indices, one for each qc log. Maybe append list with df_flux dataframe?
 for i in range(0,2):  
     sd = df.iloc[i].start_date
-    ed = df.iloc[i].end_date  
-    qc.append(np.where(((df_flux['datetime'] > sd) & (df_flux['datetime'] <= ed)),1,0))
+    ed = df.iloc[i].end_date 
+    col_idx = 'qc_flag'
+    row_idx = (df_flux['datetime'] > sd) & (df_flux['datetime'] <= ed)
+    df_flux.loc[row_idx, col_idx] = 1
+    
+df_flux.to_csv('C://Users/'+username+'/Dropbox/Obrist Lab/HarvardForestData/flux_data.csv')
 
 
